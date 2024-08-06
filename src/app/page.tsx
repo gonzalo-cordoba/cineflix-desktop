50416c43165db9a97b6ec0ab62036442406404e0
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-08-06 16:23:33 -0300

    build(app): page.tsx
    - Agregado de carrousel con los banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { Carrousel } from "@/components/component/carrousel";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to my page!</h1>
-    </div>
+    <>
+      <main>
+        <Carrousel />
+      </main>
+
+      <main className="mt-20">
+        <h1 className="font-extrabold text-3xl">
+          Descubre la cartelera en tu cine
+        </h1>
+        <Button>Cartelera</Button>
+        <Button>Próximamente</Button>
+      </main>
+    </>
   );
 }
