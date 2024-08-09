6fea35038be91172710be8e3865d57c43b739b34
Gonzalo Cordoba <GonzaloCordoba070@gmail.com>
2024-08-09 13:35:33 -0300

    build(component): footer
    - Implementación de footer v1.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/component/navbar";
+import Footer from "@/components/component/footer";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
